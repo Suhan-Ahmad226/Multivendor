e8e6e07b3209455ee1aa5a7d4d453867764a48ee
Suhan-Ahmad226 <152378845+Suhan-Ahmad226@users.noreply.github.com>
2025-09-13 10:36:10 +0600

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,22 +1,8 @@
 import axios from "axios";
-//const local = 'http://localhost:5000'
-
-const production = 'https://multivendor-vp55.onrender.com'
-
-let api_url = ''
-let mode = 'pro'
-
-if (mode === 'pro') {
-    api_url = production
-} else {
-    api_url = local
-}
-
 
 const api = axios.create({
-    baseURL : `${api_url}/api`
-})
+    baseURL: `${process.env.REACT_APP_API_URL}/api`,
+    withCredentials: true
+});
 
-
-
-export default api
+export default api;
