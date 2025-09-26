3b2d99bf5ffbb2a7bc89abd16ec1c72232bb2b24
Suhan-Ahmad226 <152378845+Suhan-Ahmad226@users.noreply.github.com>
2025-09-26 15:22:44 +0600

    Update FeatureProducts.jsx

--- frontend/src/components/products/FeatureProducts.jsx
+++ frontend/src/components/products/FeatureProducts.jsx
@@ -3,112 +3,131 @@ import { FaEye, FaRegHeart } from "react-icons/fa";
 import { RiShoppingCartLine } from "react-icons/ri";
 import Rating from '../Rating';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux';
-import { add_to_card,add_to_wishlist,messageClear } from '../../store/reducers/cardReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { add_to_card, add_to_wishlist, messageClear } from '../../store/reducers/cardReducer';
 import toast from 'react-hot-toast';
 
-const FeatureProducts = ({products}) => {
+const FeatureProducts = ({ products }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector(state => state.auth);
+  const { errorMessage, successMessage } = useSelector(state => state.card);
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const {userInfo } = useSelector(state => state.auth)
-    const {errorMessage,successMessage } = useSelector(state => state.card)
-
-    const add_card = (id) => {
-        if (userInfo) {
-           dispatch(add_to_card({
-            userId: userInfo.id,
-            quantity : 1,
-            productId : id
-           }))
-        } else {
-            navigate('/login')
-        }
+  const add_card = (id) => {
+    if (userInfo) {
+      dispatch(add_to_card({
+        userId: userInfo.id,
+        quantity: 1,
+        productId: id
+      }));
+    } else {
+      navigate('/login');
     }
+  };
 
-    useEffect(() => { 
-        if (successMessage) {
-            toast.success(successMessage)
-            dispatch(messageClear())  
-        } 
-        if (errorMessage) {
-            toast.error(errorMessage)
-            dispatch(messageClear())  
-        } 
-        
-    },[successMessage,errorMessage])
-
-
-    const add_wishlist = (pro) => {
-        dispatch(add_to_wishlist({
-            userId: userInfo.id,
-            productId: pro._id,
-            name: pro.name,
-            price: pro.price,
-            image: pro.images[0],
-            discount: pro.discount,
-            rating: pro.rating,
-            slug: pro.slug
-        }))
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
     }
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+  }, [successMessage, errorMessage]);
 
+  const add_wishlist = (pro) => {
+    if (userInfo) {
+      dispatch(add_to_wishlist({
+        userId: userInfo.id,
+        productId: pro._id,
+        name: pro.name,
+        price: pro.price,
+        image: pro.images[0],
+        discount: pro.discount,
+        rating: pro.rating,
+        slug: pro.slug
+      }));
+    } else {
+      navigate('/login');
+    }
+  };
 
-    return ( 
-        <div className='w-[85%] flex flex-wrap mx-auto'>
-            <div className='w-full'>
-            <div className='text-center flex justify-center items-center flex-col text-4xl text-slate-600 font-bold relative pb-[45px]'>
-                <h2>Feature Products </h2>
-                <div className='w-[100px] h-[2px] bg-[#059473] mt-4'></div>
-            </div>
-            </div>
+  return (
+    <div className="w-[90%] lg:w-[85%] mx-auto py-10">
+      {/* Title */}
+      <div className="text-center mb-10">
+        <h2 className="text-3xl md:text-4xl font-bold text-slate-700 relative inline-block">
+          Feature Products
+          <span className="block w-20 h-[3px] bg-[#059473] mx-auto mt-3 animate-pulse"></span>
+        </h2>
+      </div>
 
-        <div className='w-full grid grid-cols-4 md-lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6'>
-    {
-        products.map((p,i) => <div key={i} className='border group transition-all duration-500 hover:shadow-md hover:-mt-3'>
-            <div className='relative overflow-hidden'>
-            
-        {
-            p.discount ? <div className='flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2'>{p.discount}% </div> : ''
-        }
-
-        <img className='sm:w-full w-full h-[240px]' src={p.images[0]} alt="" />  
-
-        <ul className='flex transition-all duration-700 -bottom-10 justify-center items-center gap-2 absolute w-full group-hover:bottom-3'>
-            <li onClick={() => add_wishlist(p)} className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
-            <FaRegHeart />
-            </li>
-            <Link to={`/product/details/${p.slug}`} className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
-            <FaEye />
-            </Link> 
-            <li onClick={() => add_card(p._id)} className='w-[38px] h-[38px] cursor-pointer bg-white flex justify-center items-center rounded-full hover:bg-[#059473] hover:text-white hover:rotate-[720deg] transition-all'>
-            <RiShoppingCartLine />
-            </li>
-        </ul>    
-            </div>
-
-        <div className='py-3 text-slate-600 px-2'>
-            <h2 className='font-bold'>{p.name} </h2>
-            <div className='flex justify-start items-center gap-3'>
-                <span className='text-md font-semibold'>${p.price}</span>
-                <div className='flex'>
-                    <Rating ratings={p.rating} />
+      {/* Product Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+        {products.map((p, i) => (
+          <div
+            key={i}
+            className="border rounded-lg group overflow-hidden bg-white shadow-sm hover:shadow-xl transition-all duration-500 hover:-translate-y-2"
+          >
+            {/* Product Image */}
+            <div className="relative overflow-hidden">
+              {p.discount ? (
+                <div className="absolute left-3 top-3 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full shadow-md animate-bounce">
+                  -{p.discount}%
                 </div>
+              ) : null}
 
+              <img
+                src={p.images[0]}
+                alt={p.name}
+                className="w-full h-[240px] object-cover transform group-hover:scale-105 transition-transform duration-500"
+              />
+
+              {/* Hover Action Buttons */}
+              <ul className="absolute flex gap-3 justify-center items-center left-0 right-0 -bottom-12 group-hover:bottom-4 transition-all duration-500">
+                <li
+                  onClick={() => add_wishlist(p)}
+                  className="w-10 h-10 bg-white shadow rounded-full flex justify-center items-center cursor-pointer hover:bg-[#059473] hover:text-white transition-all duration-500 transform hover:rotate-[360deg]"
+                >
+                  <FaRegHeart />
+                </li>
+
+                <Link
+                  to={`/product/details/${p.slug}`}
+                  className="w-10 h-10 bg-white shadow rounded-full flex justify-center items-center cursor-pointer hover:bg-[#059473] hover:text-white transition-all duration-500 transform hover:rotate-[360deg]"
+                >
+                  <FaEye />
+                </Link>
+
+                <li
+                  onClick={() => add_card(p._id)}
+                  className="w-10 h-10 bg-white shadow rounded-full flex justify-center items-center cursor-pointer hover:bg-[#059473] hover:text-white transition-all duration-500 transform hover:rotate-[360deg]"
+                >
+                  <RiShoppingCartLine />
+                </li>
+              </ul>
             </div>
-        </div>    
 
-
-
-
-        </div>
-        )
-    }
-
-        </div>
-
-            
-        </div>
-    );
+            {/* Product Info */}
+            <div className="px-4 py-4 text-center md:text-left">
+              <h2 className="font-semibold text-lg text-slate-700 truncate hover:text-[#059473] transition">
+                {p.name}
+              </h2>
+              <div className="flex justify-center md:justify-start items-center gap-3 mt-2">
+                <span className="text-lg font-bold text-[#059473]">
+                  ${p.price}
+                </span>
+                <div className="flex">
+                  <Rating ratings={p.rating} />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FeatureProducts;
